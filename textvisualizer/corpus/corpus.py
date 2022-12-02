53f3ee901f42f1e266a4bdb3ce84b532f2abcc50
Ajdavidl <ajdavidl@yahoo.com.br>
2022-12-02 11:46:11 -0300

    Add basic wordcloud.

--- textvisualizer/corpus/corpus.py
+++ textvisualizer/corpus/corpus.py
@@ -85,3 +85,26 @@ class Corpus:
         fig = phraseNet(self.listText, connectors=connectors,
                         number_of_pairs=number_of_pairs)
         return fig
+
+    def wordcloudPlot(self, stopwords=None, max_font_size=50, max_words=100, background_color="white"):
+        """
+        Generate Word cloud figure.
+
+        It uses the wordcloud package under the hood.
+
+        Parameters
+        ----------
+        text : string
+            List of text to be used as text source in the construction of the graph.
+
+        stopwords : list of strings, default=None
+            That list is assumed to contain stop words, all of which will be removed from the resulting tokens.
+
+        max_font_size : int or None (default=50)
+            Maximum font size for the largest word. If None, height of the image is used.
+
+        max_words : number (default=100)
+            The maximum number of words.
+
+        """
+        return wordcloudPlot(' '.join(self.listText), stopwords=stopwords, max_font_size=max_font_size, max_words=max_words, background_color=background_color)
--- textvisualizer/textvisualizer.py
+++ textvisualizer/textvisualizer.py
@@ -3,9 +3,11 @@ Module for text data visualization
 """
 
 import re
+import wordcloud
 import numpy as np
 import plotly.graph_objects as go
 import networkx as nx
+import matplotlib.pyplot as plt
 from sklearn.feature_extraction.text import CountVectorizer
 
 
@@ -188,3 +190,33 @@ def phraseNet(listText, connectors, number_of_pairs=20):
     )
 
     return fig
+
+
+def wordcloudPlot(text, stopwords=None, max_font_size=50, max_words=100, background_color="white"):
+    """
+    Generate Word cloud figure.
+
+    It uses the wordcloud package under the hood.
+
+    Parameters
+    ----------
+    text : string
+        List of text to be used as text source in the construction of the graph.
+
+    stopwords : list of strings, default=None
+        That list is assumed to contain stop words, all of which will be removed from the resulting tokens.
+
+    max_font_size : int or None (default=50)
+        Maximum font size for the largest word. If None, height of the image is used.
+
+    max_words : number (default=100)
+        The maximum number of words.
+
+    """
+    cloud = wordcloud.WordCloud(stopwords=stopwords, max_font_size=max_font_size,
+                                max_words=max_words, background_color=background_color).generate(text.lower())
+
+    # Display the generated image:
+    plt.imshow(cloud, interpolation='bilinear')
+    plt.axis("off")
+    plt.show()
