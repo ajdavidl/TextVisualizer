d0b04aa668e02acfc289133db78fa9f678a4f055
Ajdavidl <ajdavidl@yahoo.com.br>
2023-02-25 15:22:05 -0300

    Add frequency tree map plot.

--- textvisualizer/frequency/frequency.py
+++ textvisualizer/frequency/frequency.py
@@ -3,8 +3,10 @@ Module for frequency plot
 """
 
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
+import plotly.express as px
 from sklearn.feature_extraction.text import CountVectorizer
 from yellowbrick.text import FreqDistVisualizer
 
@@ -171,3 +173,65 @@ def frequencyPlotYellowbrick(listText, number_of_words=20, stopwords=None, ngram
         features=features, orient='v', n=number_of_words)
     visualizer.fit(docs)
     visualizer.show()
+
+
+def frequencyTreeMap(listText, number_of_words=100, stopwords=None, ngramRange=(1, 1), vocabulary=None):
+    """
+    Plot a tree map with the token frequencies.
+
+    It receives a list of text, count the tokens and plot a tree map with the frequencies.
+
+    It uses plotly express under the hood.
+
+    Parameters
+    ----------
+    listText : list of strings
+
+    number_of_words : int
+        Number of words to be plotted.
+
+    stopwords : list of strings, default=None
+        That list is assumed to contain stop words, all of which will be removed from the resulting tokens.
+
+    ngramRange : tuple (min_n, max_n), default=(1, 1)
+        The lower and upper boundary of the range of n-values for different
+        word n-grams or char n-grams to be extracted. All values of n such
+        such that min_n <= n <= max_n will be used. For example an
+        ``ngram_range`` of ``(1, 1)`` means only unigrams, ``(1, 2)`` means
+        unigrams and bigrams, and ``(2, 2)`` means only bigrams.
+        Only applies if ``analyzer is not callable``.
+
+    vocabulary : Mapping or iterable, default=None
+        Either a Mapping (e.g., a dict) where keys are terms and values are
+        indices in the feature matrix, or an iterable over terms. If not
+        given, a vocabulary is determined from the input documents. Indices
+        in the mapping should not be repeated and should not have any gap
+        between 0 and the largest index.
+
+    Returns
+    -------
+        plotly.graph_objs._figure.Figure
+    """
+    count_vect = __countvectorize(listText, stopwords, ngramRange, vocabulary)
+    count_vect.fit(listText)
+    bag_of_words = count_vect.transform(listText)
+    sum_words = bag_of_words.sum(axis=0)
+    word_freq = [(word, sum_words[0, idx])
+                 for word, idx in count_vect.vocabulary_.items()]
+    word_freq = sorted(word_freq, key=lambda x: x[1], reverse=True)
+    y_pos = np.arange(number_of_words)
+    objects = []
+    performance = []
+    for i in range(number_of_words):
+        aux = word_freq[i]
+        objects.append(aux[0])
+        performance.append(aux[1])
+    df_words = pd.DataFrame({'words': objects, 'count': performance})
+    fig = px.treemap(df_words, path=["words"],
+                     values='count',
+                     color='count',
+                     color_continuous_scale='viridis',
+                     color_continuous_midpoint=np.average(df_words['count'])
+                     )
+    fig.update_layout(margin=dict(t=50, l=25, r=25, b=25))
+    return fig
