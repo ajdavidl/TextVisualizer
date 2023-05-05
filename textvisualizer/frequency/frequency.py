5cae887c14f0ffefc2390aba20062c38fec89118
Ajdavidl <78770941+ajdavidl@users.noreply.github.com>
2023-05-05 17:07:11 -0300

    Add a donut chart to frequency module.

--- textvisualizer/frequency/frequency.py
+++ textvisualizer/frequency/frequency.py
@@ -9,6 +9,7 @@ import plotly.graph_objects as go
 import plotly.express as px
 from sklearn.feature_extraction.text import CountVectorizer
 from yellowbrick.text import FreqDistVisualizer
+from collections import Counter
 
 
 def __countvectorize(listText, stopwords=None, ngramRange=(1, 1), vocabulary=None):
@@ -234,4 +235,67 @@ def frequencyTreeMap(listText, number_of_words=100, stopwords=None, ngramRange=(
                      color_continuous_midpoint=np.average(df_words['count'])
                      )
     fig.update_layout(margin=dict(t=50, l=25, r=25, b=25))
+    return fig
+
+
+def frequencyDonutChart(listText, number_of_words=20, stopwords=None, ngramRange=(1, 1), vocabulary=None):
+    """
+    This function takes a text as input and plots a donut chart with the word frequencies using Plotly.
+
+    Parameters
+    ----------
+    listText : list of strings
+
+    number_of_words : int
+        Number of words to be plotted.
+
+    stopwords : list of strings, default=None
+        That list is assumed to contain stop words, all of which will be removed from the resulting tokens.
+
+    ngramRange : tuple (min_n, max_n), default=(1, 1)
+        The lower and upper boundary of the range of n-values for different
+        word n-grams or char n-grams to be extracted. All values of n such
+        such that min_n <= n <= max_n will be used. For example an
+        ``ngram_range`` of ``(1, 1)`` means only unigrams, ``(1, 2)`` means
+        unigrams and bigrams, and ``(2, 2)`` means only bigrams.
+        Only applies if ``analyzer is not callable``.
+
+    vocabulary : Mapping or iterable, default=None
+        Either a Mapping (e.g., a dict) where keys are terms and values are
+        indices in the feature matrix, or an iterable over terms. If not
+        given, a vocabulary is determined from the input documents. Indices
+        in the mapping should not be repeated and should not have any gap
+        between 0 and the largest index.
+
+    Returns
+    -------
+        plotly.graph_objs._figure.Figure
+    """
+    count_vect = __countvectorize(listText, stopwords, ngramRange, vocabulary)
+    count_vect.fit(listText)
+    bag_of_words = count_vect.transform(listText)
+    sum_words = bag_of_words.sum(axis=0)
+    word_freq = [(word, sum_words[0, idx])
+                 for word, idx in count_vect.vocabulary_.items()]
+    word_freq = sorted(word_freq, key=lambda x: x[1], reverse=True)
+    top_words = {k: v for k, v in word_freq[:number_of_words]}
+
+    # Create the trace for the donut chart
+    trace = go.Pie(
+        labels=list(top_words.keys()),
+        values=list(top_words.values()),
+        hole=0.4,
+        hoverinfo='label+percent',
+        marker=dict(colors=['#66c2a5', '#fc8d62', '#8da0cb', '#e78ac3',
+                    '#a6d854', '#ffd92f', '#e5c494', '#b3b3b3', '#8dd3c7', '#bebada']),
+    )
+
+    # Create the layout for the donut chart
+    layout = go.Layout(
+        title="Word Frequencies",
+        margin=dict(t=30, b=10, l=10, r=10),
+    )
+
+    # Create the figure and plot the donut chart
+    fig = go.Figure(data=[trace], layout=layout)
     return fig
